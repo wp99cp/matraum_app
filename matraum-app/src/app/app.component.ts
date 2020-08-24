955b693f18c6bc0772f68b618e557d6a95101061
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2020-08-24 16:06:19 +0200

    basic qr code scanning

--- matraum-app/src/app/app.component.ts
+++ matraum-app/src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'matraum-app';
+
+  constructor() {
+
+
+  }
+
+  ngOnInit(): void {
+
+    // used for livestream the camera to the video element
+    const video = document.querySelector('#videoElement') as HTMLMediaElement;
+
+    // request permition
+    if (navigator.mediaDevices.getUserMedia) {
+
+      navigator.mediaDevices.getUserMedia({
+
+        // webcam settings
+        video:
+          {
+            facingMode: 'environment',
+            width: {min: 1024, ideal: 1280, max: 1920},
+            height: {min: 576, ideal: 720, max: 1080}
+          }
+
+      })
+
+        .then((stream) => {
+          video.srcObject = stream;
+        })
+
+        .catch((err0r) => {
+          console.error(err0r);
+        });
+
+    }
+
+    // start checking
+    setTimeout(() => {
+      this.check();
+    }, 2000);
+
+  }
+
+  /**
+   * Check for a QR Code in the current video snapshot.
+   * Repeats this function unit a valid QR Code is found in the image.
+   *
+   */
+  public check(): void {
+
+    setTimeout(() => {
+
+      console.log('Check for QR Code...');
+
+      const canvas = document.getElementById('canvas_temp') as HTMLCanvasElement;
+      const video = document.querySelector('#videoElement') as HTMLVideoElement;
+
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+      const context = canvas.getContext('2d');
+      const image = context.getImageData(0, 0, canvas.width, canvas.height);
+      const code = jsQR(image.data, canvas.width, canvas.height);
+
+      if (code) {
+
+        console.log('Found QR code', code);
+
+        const htmlElement = document.getElementById('QR_Code') as HTMLParagraphElement;
+        htmlElement.innerText = code.data;
+
+        return;
+
+      }
+
+      this.check();
+
+    }, 500);
+
+  }
+
 }
