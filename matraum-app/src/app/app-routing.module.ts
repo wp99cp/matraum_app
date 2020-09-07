79b819e64b685d8ce0ca79cdb45158562d417fa8
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2020-09-07 08:44:51 +0200

    add sign in page
    - add sign in page but without any sign in form... sust a button to continue

--- matraum-app/src/app/app-routing.module.ts
+++ matraum-app/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SignInPageComponent} from './pages/sign-in-page/sign-in-page.component';
+import {ScanPageComponent} from './pages/scan-page/scan-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: SignInPageComponent
+
+  },
+
+  {
+    path: 'overview-page',
+    component: ScanPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- matraum-app/src/app/app.module.ts
+++ matraum-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
+import { ScanPageComponent } from './pages/scan-page/scan-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInPageComponent,
+    ScanPageComponent
   ],
   imports: [
     BrowserModule,
