c8e865641c31a55623f88f415dc31889e9b8bba5
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2021-03-18 13:41:46 +0100

    Support for HTC added (#6)
    * add support for htc
    
    * increase version number

--- matraum-app/src/app/webcam.service.ts
+++ matraum-app/src/app/webcam.service.ts
@@ -7,7 +7,10 @@ export class WebcamService {
 
   private stream: MediaStream;
 
-  constructor() {  }
+  constructor() {
+
+
+  }
 
   public async getMediaStream(): Promise<MediaStream> {
 
@@ -19,8 +22,8 @@ export class WebcamService {
         video: {
           // the browser will try to honor this resolution, but it may end up being lower.
           facingMode: 'environment',
-          width: {min: 100, ideal: 320, max: 500},
-          height: {min: 100, ideal: 320, max: 500}
+          width: {min: 100, max: 320},
+          height: {min: 100, max: 320}
         }
       };
 
